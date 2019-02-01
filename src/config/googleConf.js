6c634b0967b1c6dfaad85aae835d912b866b1f1d
ChanAhn <moz5691@gmail.com>
2019-02-01 14:48:04 -0500

    init

--- src/config/googleConf.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDmje-EVNvRkvGei7lXVm8gWNjjsNtlRNc",
-  authDomain: "inventory-a8be8.firebaseapp.com",
-  databaseURL: "https://inventory-a8be8.firebaseio.com",
-  projectId: "inventory-a8be8",
-  storageBucket: "inventory-a8be8.appspot.com",
-  messagingSenderId: "1004312960483"
-}
